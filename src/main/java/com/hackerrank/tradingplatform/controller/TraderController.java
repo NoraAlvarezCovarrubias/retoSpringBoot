187ea46ed2ebd29e8bebb1a11780bbda28f2d59b
hackerrank <me@hackerrank.com>
2021-10-22 12:33:15 -0500

    primer reto

--- src/main/java/com/hackerrank/tradingplatform/controller/TraderController.java
+++ src/main/java/com/hackerrank/tradingplatform/controller/TraderController.java
@@ -7,10 +7,12 @@ import com.hackerrank.tradingplatform.model.Trader;
 import com.hackerrank.tradingplatform.service.TraderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.stream.Collectors.toList;
 
@@ -23,8 +25,14 @@ public class TraderController {
     //register
     @RequestMapping(value = "/register", method = RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerTrader(@RequestBody @Valid Trader trader) {
+    public ResponseEntity registerTrader(@RequestBody @Valid Trader trader) {
+    	Optional<Trader> email = traderService.getByEmail(trader.getEmail());
+    	if(email.isPresent()) {
+    		return ResponseEntity.badRequest().build();
+    	}
+    	
         traderService.registerTrader(trader);
+        return ResponseEntity.created(null).build();
     }
 
     //get by email
--- src/main/java/com/hackerrank/tradingplatform/service/TraderService.java
+++ src/main/java/com/hackerrank/tradingplatform/service/TraderService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TraderService {
@@ -24,6 +25,10 @@ public class TraderService {
 
     public Trader getTraderByEmail(String email) {
         return traderRepository.findByEmail(email).orElse(new Trader());
+    }
+    
+    public Optional<Trader> getByEmail(String email) {
+        return traderRepository.findByEmail(email);
     }
 
     public List<Trader> getAllTraders() {
